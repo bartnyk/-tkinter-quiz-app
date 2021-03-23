143e3b12aea1c43ab6721209e618f0f4e70aa21f
bartnyk <kuba.bartnik114@gmail.com>
2021-03-23 22:35:39 +0100

    fixed issues, everything looks fine.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import requests
 from questions_db_api import GetQuestions
+from time import sleep
 
 CATEGORIES = requests.get("https://opentdb.com/api_category.php").json()
 
@@ -15,26 +16,64 @@ window = Tk()
 window.title("Quiz")
 window.config(padx=40, pady=60, bg=PLATE_COLOR)
 
+end = False
+
 def main():
+    def finish():
+        global end
+        end = True
+        plate.itemconfigure(question_window, text=f"\tTHE END\t\nYour score: {questions.score}/{params['amount']} ({(questions.score/params['amount'])*100}%)\nDo you want to play again?")
     def answer(pick):
-        questions.check_answer(window, plate, pick)
+        true_pick.config(state='disabled')
+        false_pick.config(state='disabled')
+        def change_question():
+            true_pick.config(state='normal')
+            false_pick.config(state='normal')
+            if questions.i < params['amount']:
+                plate.config(bg=PLATE_COLOR)
+                plate.itemconfigure(question_window, text=questions.print_question())
+            else:
+                finish()
+        if not end:
+            if questions.check_answer(pick):
+                plate.config(bg=TRUE_COLOR)
+                questions.score += 1
+            else:
+                plate.config(bg=FALSE_COLOR)
+        else:
+            if pick == "True":
+                questions.i = 0
+                blackout()
+                return set_params()
+            else:
+                window.destroy()
+        scoreboard.config(text=f'Score: {questions.score}')
+        window.after(2000, change_question)
 
     questions = GetQuestions(params)
     window.config(bg=BG_COLOR)
+    scoreboard = Label(text=f"Score: 0", font=("Comic Sans MS", 16, "bold"), bg=BG_COLOR, fg="white")
+    scoreboard.grid(row=0, column=1)
     plate = Canvas(width=600, height=500, bg=PLATE_COLOR)
     question_window = plate.create_text(300, 250, width=500, text=questions.print_question(), fill="white", font=("Comic Sans MS", 16, "bold"))
     true_pick = Button(bg=BG_COLOR, image=true, highlightthickness=0, command= lambda: answer("True"))
     false_pick = Button(bg=BG_COLOR, image=false, highlightthickness=0, command= lambda: answer("False"))
-    plate.grid(row=0, columnspan=2)
-    true_pick.grid(row=1, column=0, pady=(30, 0))
-    false_pick.grid(row=1, column=1, pady=(30, 0))
+    plate.grid(row=1, columnspan=2)
+    true_pick.grid(row=2, column=0, pady=(30, 0))
+    false_pick.grid(row=2, column=1, pady=(30, 0))
+
+def blackout():
+    global end
+    for widget in window.winfo_children():
+        widget.destroy()
+    window.config(bg=PLATE_COLOR)
+    end = False
 
 def choosen_category(category):
     global params
     params['category'] = category
-    for widget in window.winfo_children():
-        widget.destroy()
-    main()
+    blackout()
+    return main()
 
 def set_amount(amount):
     global params
